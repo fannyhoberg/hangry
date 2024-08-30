bdd48d31fba582af9013a5e69c2a9474494f0892
Fanny Hoberg <fannyh.92@hotmail.com>
2024-08-30 15:31:37 +0200

    added email in navabar if username does not exist, and changed the styling a bit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,54 +6,48 @@ import useAuth from "../hooks/useAuth";
 import { Image, NavDropdown } from "react-bootstrap";
 
 const Navigation = () => {
-  const { currentUser, userPhotoUrl, userName } = useAuth();
+  const { currentUser, userPhotoUrl, userName, userEmail } = useAuth();
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar className="bg-body-tertiary">
       <Container>
         <Navbar.Brand as={Link} to="/" className="logo">
           Hangry
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {currentUser ? (
-              <>
-                <NavDropdown
-                  title={
-                    userPhotoUrl ? (
-                      <Image
-                        src={userPhotoUrl}
-                        height={30}
-                        width={30}
-                        roundedCircle
-                      />
-                    ) : (
-                      userName
-                    )
-                  }
-                >
-                  <NavDropdown.Item as={NavLink} end to="/add-establishment">
-                    Add establishment
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={NavLink} end to="/update-profile">
-                    Update profile
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={NavLink} end to="/logout">
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </>
-            ) : (
-              <>
-                {" "}
-                <Nav.Link as={NavLink} end to="/login">
-                  Login{" "}
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+        <Nav className="ms-auto">
+          {currentUser ? (
+            <>
+              <NavDropdown
+                title={
+                  userPhotoUrl ? (
+                    <Image
+                      src={userPhotoUrl}
+                      height={30}
+                      width={30}
+                      roundedCircle
+                    />
+                  ) : (
+                    userName || userEmail
+                  )
+                }
+              >
+                <NavDropdown.Item as={NavLink} end to="/add-establishment">
+                  Add establishment
+                </NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} end to="/update-profile">
+                  Update profile
+                </NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} end to="/logout">
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </>
+          ) : (
+            <Nav.Link as={NavLink} end to="/login">
+              Login
+            </Nav.Link>
+          )}
+        </Nav>
       </Container>
     </Navbar>
   );
