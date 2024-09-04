8d6d72218ce038d9fe92226c4f546fe72e87ec46
Hanna <hms.soderholm@gmail.com>
2024-09-04 18:58:30 +0200

    added image carousel to list view of establishments on map page

---
+++ src/components/map/CardCarousel.tsx
@@ -0,0 +1,24 @@
+import Image from "react-bootstrap/Image";
+import Carousel from "react-bootstrap/Carousel";
+
+interface CarouselProps {
+  establishmentName: string;
+  photoUrls: string[];
+}
+
+const CardCarousel: React.FC<CarouselProps> = ({ establishmentName, photoUrls }) => {
+  return (
+    <Carousel fade>
+      {photoUrls &&
+        photoUrls.map((url) => (
+          <Carousel.Item key={url}>
+            <div className="carousel-slide">
+              <Image src={url} className="carousel-img" alt={`Image of ${establishmentName}`} />
+            </div>
+          </Carousel.Item>
+        ))}
+    </Carousel>
+  );
+};
+
+export default CardCarousel;
