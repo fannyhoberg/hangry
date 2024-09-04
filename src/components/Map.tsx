a18e741ffa57668e8aacb2a42c9dcbb250bcb8fb
Josefin Ahlenius <josefinlouahlenius@gmail.com>
2024-09-04 19:03:06 +0200

    Used ListCard instead of google maps InfoWindow component. Different based on mobile view or desktop.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -29,14 +29,15 @@ const Map = () => {
   const [info, setInfo] = useState<Establishment | null>(null);
   const [centerPosition, setCenterPosition] = useState<PositionCoords>(defaultCenter);
   const [showList, setShowList] = useState<boolean>(false);
+  const [map, setMap] = useState<google.maps.Map | null>(null);
 
   const locations = useMemo(
     () => ({
       MyPosition: userLocation
         ? {
-            lat: userLocation.geolocation.coords.latitude,
-            lng: userLocation.geolocation.coords.longitude,
-          }
+          lat: userLocation.geolocation.coords.latitude,
+          lng: userLocation.geolocation.coords.longitude,
+        }
         : defaultCenter,
       Lund: { lat: 55.7046601, lng: 13.1910073 },
       Malmö: { lat: 55.6052931, lng: 13.0001566 },
@@ -112,6 +113,10 @@ const Map = () => {
     }
   }, [searchParams, city, locations]);
 
+  const onLoad = useCallback((mapInstance: google.maps.Map) => {
+    setMap(mapInstance);  // Save map instance in state
+  }, []);
+
   if (loading || !centerPosition) {
     console.log("Loading data and location...");
     return <div>Loading map...</div>;
@@ -131,14 +136,15 @@ const Map = () => {
         )}
       </div>
       <div className="map-wrapper">
-        <GoogleMap mapContainerStyle={containerStyle} center={centerPosition} zoom={14}>
+        <GoogleMap mapContainerStyle={containerStyle} center={centerPosition} zoom={14} onLoad={onLoad}>
           <>
             {showInfoWindow && infoWindowPosition && info && (
               <MarkerInfoWindow
                 handleClose={handleClose}
-                position={infoWindowPosition}
-                info={info}
                 centerPosition={centerPosition}
+                info={info}
+                map={map}
+                position={infoWindowPosition}
               />
             )}
 
