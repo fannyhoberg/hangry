14af7922c9ad193be76f6afd625fa0bf1fe45fbd
Fanny Hoberg <fannyh.92@hotmail.com>
2024-09-04 17:17:32 +0200

    changed icon for current position marker 👹

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -18,6 +18,14 @@ const defaultCenter: PositionCoords = {
   lng: 13.001277692558267,
 };
 
+const createIcon = (text: string) =>
+  `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(`
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40">
+      <circle cx="20" cy="20" r="18" fill="#5EA38A" stroke="#004F32" stroke-width="3" />
+      <text x="20" y="27" font-size="24" text-anchor="middle" fill="#004F32" font-family="Arial" font-weight="bold">${text}</text>
+    </svg>
+  `)}`;
+
 const Map = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const initialCity = searchParams.get("city") || "Malmö";
@@ -25,10 +33,16 @@ const Map = () => {
   const { data: establishments, loading } = useGetEstablishmentsByCity(city);
   const { userLocation } = useGetUserLocation();
   const [showInfoWindow, setShowInfoWindow] = useState(false);
-  const [infoWindowPosition, setInfoWindowPosition] = useState<PositionCoords | null>(null);
+  const [infoWindowPosition, setInfoWindowPosition] =
+    useState<PositionCoords | null>(null);
   const [info, setInfo] = useState<Establishment | null>(null);
-  const [centerPosition, setCenterPosition] = useState<PositionCoords>(defaultCenter);
+  const [centerPosition, setCenterPosition] =
+    useState<PositionCoords>(defaultCenter);
   const [showList, setShowList] = useState<boolean>(false);
+
+  const coffeeIcon = useMemo(() => createIcon("☕"), []);
+  const restaurantIcon = useMemo(() => createIcon("🍕"), []);
+  const barIcon = useMemo(() => createIcon("🍺"), []);
 
   const locations = useMemo(
     () => ({
@@ -43,28 +57,6 @@ const Map = () => {
       Eslöv: { lat: 55.83900838618268, lng: 13.30492141526424 },
     }),
     [userLocation]
-  );
-
-  const coffeeIcon = useMemo(
-    () =>
-      `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(`
-    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40">
-      <circle cx="20" cy="20" r="18" fill="#5EA38A" stroke="#004F32" stroke-width="3" />
-      <text x="20" y="27" font-size="25" text-anchor="middle" fill="#004F32" font-family="Arial" font-weight="bold">&#x2615;</text>
-    </svg>
-  `)}`,
-    []
-  );
-
-  const restaurantIcon = useMemo(
-    () =>
-      `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(`
-    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40">
-      <circle cx="20" cy="20" r="18" fill="#5EA38A" stroke="#004F32" stroke-width="3" />
-      <text x="20" y="27" font-size="24" text-anchor="middle" fill="#004F32" font-family="Arial" font-weight="bold">&#127790;</text>
-    </svg>
-  `)}`,
-    []
   );
 
   const handleMarkerClick = useCallback(
@@ -87,7 +79,11 @@ const Map = () => {
       const newCenter = locations[value as keyof typeof locations];
       setCenterPosition(newCenter);
       const newCity =
-        value === "MyPosition" ? (userLocation ? userLocation.cityName : "Malmö") : value;
+        value === "MyPosition"
+          ? userLocation
+            ? userLocation.cityName
+            : "Malmö"
+          : value;
       setCity(newCity);
       setSearchParams({ city: newCity });
     },
@@ -120,7 +116,10 @@ const Map = () => {
   return (
     <>
       <div>
-        <select onChange={handleLocationChange} style={{ padding: "8px", fontSize: "16px" }}>
+        <select
+          onChange={handleLocationChange}
+          style={{ padding: "8px", fontSize: "16px" }}
+        >
           <option value="MyPosition">My position</option>
           <option value="Lund">Lund</option>
           <option value="Malmö">Malmö</option>
@@ -131,7 +130,11 @@ const Map = () => {
         )}
       </div>
       <div className="map-wrapper">
-        <GoogleMap mapContainerStyle={containerStyle} center={centerPosition} zoom={14}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={centerPosition}
+          zoom={14}
+        >
           <>
             {showInfoWindow && infoWindowPosition && info && (
               <MarkerInfoWindow
@@ -148,6 +151,8 @@ const Map = () => {
                   lat: userLocation.geolocation.coords.latitude,
                   lng: userLocation.geolocation.coords.longitude,
                 }}
+                zIndex={2000}
+                icon={"https://fav.farm/👹"}
               />
             )}
 
@@ -163,7 +168,13 @@ const Map = () => {
                     onClick={() => handleMarkerClick(position, establishment)}
                     key={establishment._id}
                     position={position}
-                    icon={establishment.category[0] === "restaurant" ? restaurantIcon : coffeeIcon}
+                    icon={
+                      establishment.category.includes("bar")
+                        ? barIcon
+                        : establishment.category.includes("restaurant")
+                        ? restaurantIcon
+                        : coffeeIcon
+                    }
                   />
                 );
               })}
